4fcdb814a09e13ee93acb41c1e368fd90f107fbb
Oleh3131 <oshevskyioleh@gmail.com>
2022-02-17 19:25:38 +0300

    add info

--- classwork1/App.js
+++ classwork1/App.js
@@ -3,12 +3,39 @@ const path = require('path');
 
 // Завдання на практику
 
+
 // 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і одразу,
 //     дані які ви отримали запишіть їх в інший файл, в вас вийде невеликий callback hell, пізніше я вам покажу
 //     як можна це обійти, але поки зробіть так
 
 
+// const firstTask = () => {
+//     fs.writeFile(path.join(__dirname, 'fitstTask.txt'), '\nFIRST TASK INFO', (err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//         fs.readFile(path.join(__dirname, 'fitstTask.txt'), 'utf-8', (eer, data) => {
+//             if (err) {
+//                 console.log(err);
+//                 throw err;
+//             }
+//             console.log(data);
+//             fs.writeFile(path.join(__dirname, 'fitstTaskNext.txt'), `${data}`, (err) => {
+//                 if (err) {
+//                     console.log(err);
+//                     throw err;
+//                 }
+//             })
+//         })
+//     });
+// }
+//
+//
+// firstTask()
 
+
+// --------------------------------------------------------------------------------------------------------------------------
 
 
 //
@@ -16,6 +43,41 @@ const path = require('path');
 // Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку
 // та файл в ній, старий файл видаліть після того як все завершиться.
 //     Також вийде callback hell
+
+
+fs.writeFile(path.join(__dirname, "secondTask.txt"), '\nSECOND TASK INFO', (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    fs.readFile(path.join(__dirname, "secondTask.txt"), 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        console.log(data);
+        fs.mkdir(path.join(__dirname, 'SecondTask'), (err) => {
+            if (err) {
+                console.log(err);
+            }
+            fs.writeFile(path.join(__dirname,'SecondTask', "secondTaskNext.txt"), `${data}`, (err) => {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+                fs.unlink(path.join(__dirname, "secondTask.txt"), (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            })
+        })
+    })
+});
+
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
 //
 // 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані
 // (можуть бути нові папки і файли(в файли запишіть якусь дату) )
@@ -24,59 +86,59 @@ const path = require('path');
 //     вам потрібно їх перейменувати і додати до назви префікс _new
 
 
-fs.mkdir(path.join(__dirname, 'ThirdTask'), (err) => {
-    if (err) {
-        console.log(err);
-    }
-    fs.mkdir(path.join(__dirname, 'ThirdTask', 'ThirdTaskNext'), (err) => {
-        if (err) {
-            console.log(err);
-        }
-        fs.writeFile(path.join(__dirname, 'ThirdTask', 'thirdTask.txt'), `\nTHIRD TASK INFO`, (err) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-        })
-    })
-});
-
-
-function directory() {
-
-    fs.readdir(path.join(__dirname, 'ThirdTask'), (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-
-        for (let file of data) {
-            fs.stat(__dirname+'/ThirdTask'+ `/${file}`,  (err, stats)=>{
-                if (err){
-                    console.log(err);
-                    throw err;
-                }
-                else if (stats.isFile()) {
-                    fs.appendFile(path.join(__dirname,'ThirdTask',`${file}`), '', (err) => {
-                        if (err) {
-                            console.log(err);
-                            throw err;
-                        }
-                    })
-                }
-                else if (stats.isDirectory()) {
-                    fs.rename(path.join(__dirname,'ThirdTask',`${file}`), path.join(__dirname,'ThirdTask',`/New_${file}`), (err) => {
-                        if (err) {
-                            console.log(err);
-                            throw err
-                        }
-                    })
-                }
-            });
-        }
-    })
-}
-
-directory();
+// fs.mkdir(path.join(__dirname, 'ThirdTask'), (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     fs.mkdir(path.join(__dirname, 'ThirdTask', 'ThirdTaskNext'), (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         fs.writeFile(path.join(__dirname, 'ThirdTask', 'thirdTask.txt'), `\nTHIRD TASK INFO`, (err) => {
+//             if (err) {
+//                 console.log(err);
+//                 throw err;
+//             }
+//         })
+//     })
+// });
+//
+//
+// function directory() {
+//
+//     fs.readdir(path.join(__dirname, 'ThirdTask'), (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//
+//         for (let file of data) {
+//             fs.stat(__dirname+'/ThirdTask'+ `/${file}`,  (err, stats)=>{
+//                 if (err){
+//                     console.log(err);
+//                     throw err;
+//                 }
+//                 else if (stats.isFile()) {
+//                     fs.appendFile(path.join(__dirname,'ThirdTask',`${file}`), '', (err) => {
+//                         if (err) {
+//                             console.log(err);
+//                             throw err;
+//                         }
+//                     })
+//                 }
+//                 else if (stats.isDirectory()) {
+//                     fs.rename(path.join(__dirname,'ThirdTask',`${file}`), path.join(__dirname,'ThirdTask',`/New_${file}`), (err) => {
+//                         if (err) {
+//                             console.log(err);
+//                             throw err
+//                         }
+//                     })
+//                 }
+//             });
+//         }
+//     })
+// }
+//
+// directory();
 
 
 // -----------------------------------------------------------------------------------------------------------------------------
